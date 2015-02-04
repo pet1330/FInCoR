e7677d509687db587b54b6f1de5aeee7b69e5a0a
Peter Lightbody <pet1330@gmail.com>
2015-02-04 17:22:41 +0000

    flipped pointcloud correct way up...

--- src/camera_setup/src/multi_cam_calibration.py
+++ src/camera_setup/src/multi_cam_calibration.py
@@ -5,6 +5,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 import tf
+import threading
 
 class calibrate_camera:
 
@@ -14,13 +15,14 @@ class calibrate_camera:
         self.right_image = rospy.Subscriber('/right/rgb/image_color', Image, self.right_callback)
         self.br = tf.TransformBroadcaster()
         self.listener = tf.TransformListener()
+        self.lock = threading.Lock()
 
-        square_size = 0.05
         self.pattern_size = (6, 8)
         self.pattern_points = np.zeros( (np.prod(self.pattern_size), 3), np.float32)
         self.pattern_points[:,:2] = np.indices(self.pattern_size).T.reshape(-1, 2)
-        self.pattern_points *= square_size
-    
+        self.pattern_points[:,0]=-self.pattern_points[:,0]
+        self.pattern_points *= 0.05
+        
     def find_tf(self, ros_image_msg, invert):
         try:
             #Colour Image
@@ -37,6 +39,7 @@ class calibrate_camera:
             if invert:
                 img_points = np.flipud(np.fliplr(img_points))
             obj_points.append(self.pattern_points)
+            #print obj_points
             camera_matrix=np.array([525.0, 0.0, 319.5, 0.0, 525.0, 239.5, 0.0, 0.0, 1.0]).reshape(3,3)
             dist_coefs=np.empty((5,1))
             _, rVec, tVec = cv2.solvePnP(np.array(obj_points),np.array(img_points),camera_matrix,dist_coefs)
@@ -50,19 +53,22 @@ class calibrate_camera:
                 try:
                     (transr,rotr) = self.listener.lookupTransform('/marker_right', '/right/right_link', rospy.Time(0))
                     self.br.sendTransform(transr,rotr,rospy.Time.now(), '/right/right_link', "/base")
-                    print "no error right"
+                    #print "no error right"
                 except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                     pass
-                
+                #with self.lock:
+                   # print "Righ", tVec.reshape(1,-1) , q
             else:
                 self.br.sendTransform((tVec[0],tVec[1] , tVec[2]),q,rospy.Time.now(), "/marker_left", "/left/left_rgb_optical_frame")
                 try:
                     (transl,rotl) = self.listener.lookupTransform('/marker_left', '/left/left_link', rospy.Time(0))
                     self.br.sendTransform(transl,rotl,rospy.Time.now(), '/left/left_link', "/base")
-                    print "no error left"
-
+                    #print "no error left"
                 except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                     pass
+                #with self.lock:
+                   # print "Left", tVec.reshape(1,-1) , q
+
 
     def left_callback(self,left):
         self.find_tf(left,invert = False)
@@ -77,3 +83,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
