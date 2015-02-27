abbff859e5b3285c769f0f96827e7e4d6245ac9e
Peter Lightbody <pet1330@gmail.com>
2015-02-27 17:22:15 +0000

    circle gen needs fixing

--- src/circle_detection/src/generate_circle.py
+++ src/circle_detection/src/generate_circle.py
@@ -1,47 +1,55 @@
 import cv2
 from fpdf import FPDF
+import math
 import numpy as np
-import os
 import sys
 
-numOfCircles = 150
-circleDiameter = 50; #in mm
+circleDiameter = 50
 
-xPos = 0;
-yPos = 0;
-
-
-pdf = FPDF()
-pdf.alias_nb_pages()
-pdf.add_page()
-
+numOfCircles = 10
 numberPerRow = (210 / (circleDiameter + (circleDiameter / 5)))
 numberPerColum = (297 / (circleDiameter + (circleDiameter / 5)))
-print '#######################################'
-print numberPerRow, numberPerColum
-print '#######################################'
 
-img = np.ones((512, 512, 3), np.uint8)
-for i in range(40, numOfCircles + 40):
-    img[:, :] = (255, 255, 255)
-    for draw_i in range(0, 128-i):
-        cv2.circle(img, (256, 256), 252-(draw_i * 2), (0, 0, 0), 2)
-    cv2.imwrite("circle.png", img)
-    cv2.imshow('image', img)
-    #k = cv2.waitKey(1000) & 0xFF
+
+
+def gen_single_circle(outsideCircleRad, insideCircleRad):
     
-    pdf.image("circle.png", (xPos * (circleDiameter + (circleDiameter / 5)))-circleDiameter, (yPos * (circleDiameter + (circleDiameter / 5)))-circleDiameter, circleDiameter, circleDiameter, 'png')
-    #print (xPos * (circleDiameter + (circleDiameter / 5)))-circleDiameter, (yPos * (circleDiameter + (circleDiameter / 5)))-circleDiameter
+    scale = outsideCircleRad;
+    
+    img = np.ones(((scale * 2) + 10, (scale * 2) + 10, 3), np.uint8)
+    img[:,:] = (255, 255, 255)
+    
+    for i in range(outsideCircleRad, insideCircleRad, -2):
+        cv2.circle(img, (scale + 5, scale + 5), i, (0, 0, 0), 2)
+    return img
 
-    xPos = (i % numberPerRow) + 1
-    sys.stdout.write(str(xPos) + "  ")
-    if (numberPerRow - xPos) == 0:
-        yPos += 1
-        print
+
+def gen_pdf():
+    
+    pdf = FPDF()
+    pdf.add_page()
+
+    for i in range(1, numOfCircles):
+        xPos = 0
+        yPos = 0        
+        cv2.imwrite('circle.png', gen_single_circle(circleDiameter, 30))
+        
+        xPos += 1
+        if xPos > numberPerRow:
+            yPos += 1
+            xPos = 0
         if yPos > numberPerColum:
             pdf.add_page()
             xPos = 0
             yPos = 0
-            print "--------------"
-    
-pdf.output('BETA.pdf', 'F')
+        
+        pdf.image('circle.png', (xPos * (circleDiameter + (circleDiameter / 5)))-circleDiameter, (yPos * (circleDiameter + (circleDiameter / 5)))-circleDiameter, circleDiameter, circleDiameter, 'png')
+
+        #cv2.imshow('image', a);
+        #k = cv2.waitKey(1000) & 0xFF
+
+    pdf.output('BETA.pdf', 'F')
+        
+gen_pdf()
+#cv2.imshow('image', a);
+#k = cv2.waitKey(1000) & 0xFF
