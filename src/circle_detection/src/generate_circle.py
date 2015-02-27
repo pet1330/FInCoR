4c02a43c5d3b8b4345a15c164f4ce967d8ba2628
Peter Lightbody <pet1330@gmail.com>
2015-02-27 12:41:20 +0000

    circle generator works

--- src/circle_detection/src/generate_circle.py
+++ src/circle_detection/src/generate_circle.py
@@ -2,51 +2,46 @@ import cv2
 from fpdf import FPDF
 import numpy as np
 import os
+import sys
 
-numOfCircles = 3
-circleDiameter = 0.05;
+numOfCircles = 150
+circleDiameter = 50; #in mm
+
+xPos = 0;
+yPos = 0;
 
 
 pdf = FPDF()
+pdf.alias_nb_pages()
 pdf.add_page()
-numberPerRow = 0.21 / circleDiameter
 
-print "ROW", numberPerRow
+numberPerRow = (210 / (circleDiameter + (circleDiameter / 5)))
+numberPerColum = (297 / (circleDiameter + (circleDiameter / 5)))
+print '#######################################'
+print numberPerRow, numberPerColum
+print '#######################################'
 
 img = np.ones((512, 512, 3), np.uint8)
 for i in range(40, numOfCircles + 40):
     img[:, :] = (255, 255, 255)
     for draw_i in range(0, 128-i):
-        cv2.circle(img, (256, 256), 256-(draw_i * 2), (0, 153, 255), 2)
+        cv2.circle(img, (256, 256), 252-(draw_i * 2), (0, 0, 0), 2)
     cv2.imwrite("circle.png", img)
     cv2.imshow('image', img)
-    k = cv2.waitKey(1000) #& 0xFF
-    pdf.image("circle.png", (numOfCircles / 4) * 50, (i / 4), 50, 50, 'png')
-    col = i / numberPerRow
-    row = i - col * numberPerRow
+    #k = cv2.waitKey(1000) & 0xFF
+    
+    pdf.image("circle.png", (xPos * (circleDiameter + (circleDiameter / 5)))-circleDiameter, (yPos * (circleDiameter + (circleDiameter / 5)))-circleDiameter, circleDiameter, circleDiameter, 'png')
+    #print (xPos * (circleDiameter + (circleDiameter / 5)))-circleDiameter, (yPos * (circleDiameter + (circleDiameter / 5)))-circleDiameter
 
-    print col, row
+    xPos = (i % numberPerRow) + 1
+    sys.stdout.write(str(xPos) + "  ")
+    if (numberPerRow - xPos) == 0:
+        yPos += 1
+        print
+        if yPos > numberPerColum:
+            pdf.add_page()
+            xPos = 0
+            yPos = 0
+            print "--------------"
     
 pdf.output('BETA.pdf', 'F')
-########################################################################################################################################
-
-#for i in range(0, numberOfCircles):
-#    img[:, :] = (255, 255, 255)
-
-    #for draw_i in range(0, 128):
-    #    cv2.circle(img, 10, 10, 10, (0, 0, 255), 1)
-# cv2.imshow('image', img)
-    #  k = cv2.waitKey(100) #& 0xFF
-#   cv2.imwrite("circle.png", img)
-    #   pdf.image("circle.png", x, y, circleDiameter, circleDiameter, 'png')
-#    try:
-#os.remove("/home/peter/FInCoR/src/circle_detection/src/circle_image/i" + str(2-9) + ".png")
-    #   except: 
-        #      pass
-#pdf.output('BETA.pdf', 'F')
-
-
-
-
-
-
