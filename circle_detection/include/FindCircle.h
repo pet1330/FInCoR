e10235f6dc9cf9124f35f986912c58b7aa6f972d
Peter Lightbody <pet1330@gmail.com>
2015-04-28 17:53:39 +0100

    Split FindCircle into h and cpp and added uuid

---
+++ circle_detection/include/FindCircle.h
@@ -0,0 +1,73 @@
+#ifndef __FIND_CIRCLE_H__
+#define __FIND_CIRCLE_H__
+
+#include "CCircleDetect.h"
+#include "CTransformation.h"
+#include "ros/ros.h"
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/image_encodings.h>
+#include <geometry_msgs/Pose.h>
+#include <tf/LinearMath/QuadWord.h>
+#include <geometry_msgs/PointStamped.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <tf/transform_broadcaster.h>
+#include "circle_detection/STrackedObject.h"
+#include <tf/transform_listener.h>
+#include <tf/transform_datatypes.h>
+#include <sensor_msgs/Image.h>
+#include <math.h>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <map>
+#include <string>
+//#include<iostream>
+
+#define MAX_PATTERNS 6
+
+class FindCircle {
+public:
+
+
+
+    int defaultImageWidth;
+    int defaultImageHeight;
+    float circleDiameter;
+
+    void cameraInfoCallBack(const sensor_msgs::CameraInfo &msg);
+    void imageCallback(const sensor_msgs::ImageConstPtr& msg);
+    void init(int argc, char* argv[]);
+    FindCircle(void);
+    ~FindCircle(void);
+
+private:
+
+    // Generate universally unique ID
+    template<typename T>
+    std::string num_to_str(T num) {std::stringstream ss; ss << num; return ss.str();}
+    std::string startup_time_str;
+    boost::uuids::uuid dns_namespace_uuid;
+    std::string generateUUID(std::string time, int id);
+
+    ros::NodeHandle *nh;
+    image_transport::Publisher imdebug;
+    tf::TransformListener* lookup;
+    ros::Publisher pubLeft, pubRight, vis_pub;
+
+    // Tracking Code
+    std::clock_t start;
+    std::string topic;
+    CRawImage *image;
+    CCircleDetect *detectorArray[MAX_PATTERNS];
+    STrackedObject objectArray[MAX_PATTERNS];
+    STrackedObject objectArray3D[MAX_PATTERNS];
+    SSegment currentSegmentArray[MAX_PATTERNS];
+    SSegment lastSegmentArray[MAX_PATTERNS];
+
+    //3D transform code
+    CTransformation *trans;
+};
+
+#endif
--- circle_detection/src/FindCircle.cpp
+++ circle_detection/src/FindCircle.cpp
@@ -1,57 +1,13 @@
-#include "CCircleDetect.h"
-#include "CTransformation.h"
-#include "ros/ros.h"
-#include <image_transport/image_transport.h>
-#include <sensor_msgs/image_encodings.h>
-#include <geometry_msgs/Pose.h>
-#include <tf/LinearMath/QuadWord.h>
-#include <geometry_msgs/PointStamped.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <visualization_msgs/Marker.h>
-#include <visualization_msgs/MarkerArray.h>
-#include <tf/transform_broadcaster.h>
-#include "circle_detection/STrackedObject.h"
-#include <tf/transform_listener.h>
-#include <tf/transform_datatypes.h>
-#include <sensor_msgs/Image.h>
-#include <math.h>
+#include "FindCircle.h"
 
-
-#define MAX_PATTERNS 6
-
-int defaultImageWidth = 640;
-int defaultImageHeight = 480;
-float circleDiameter = 0.049;
-float rotateBy = 0;
-
-ros::NodeHandle *nh;
-image_transport::Publisher imdebug;
-tf::TransformListener* lookup;
-ros::Publisher pubLeft, pubRight, vis_pub;
-
-std::clock_t start;
-std::string topic;
-
-CRawImage *image;
-
-CCircleDetect *detectorArray[MAX_PATTERNS];
-STrackedObject objectArray[MAX_PATTERNS];
-STrackedObject objectArray3D[MAX_PATTERNS];
-SSegment currentSegmentArray[MAX_PATTERNS];
-SSegment lastSegmentArray[MAX_PATTERNS];
-CTransformation *trans;
-
-std::string itemLookUp() {
-
-    /*
-     *
-     * Give each circle a name or ID based on the ratio.
-     * 
-     */
-
+std::string FindCircle::generateUUID(std::string time, int id) {
+    boost::uuids::name_generator gen(dns_namespace_uuid);
+    time += num_to_str<int>(id);
+    return num_to_str<boost::uuids::uuid>(gen(time.c_str()));
 }
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
+void FindCircle::imageCallback(const sensor_msgs::ImageConstPtr& msg) {
+
     if (image->bpp != msg->step / msg->width || image->width != msg->width || image->height != msg->height) {
         delete image;
         ROS_DEBUG("Readjusting image format from %ix%i %ibpp, to %ix%i %ibpp.", image->width, image->height, image->bpp, msg->width, msg->height, msg->step / msg->width);
@@ -72,32 +28,42 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
 
         if (currentSegmentArray[i].valid) {
             objectArray[i] = trans->transform(currentSegmentArray[i]);
-            
-            if(isnan(objectArray[i].x)) continue;
-            if(isnan(objectArray[i].y)) continue;
-            if(isnan(objectArray[i].z)) continue;
-            if(isnan(objectArray[i].roll)) continue;
-            if(isnan(objectArray[i].pitch)) continue;
-            if(isnan(objectArray[i].yaw)) continue;
 
-            // printf("X:%f  Y:%f Z:%f R:%f\n", objectArray[i].x, objectArray[i].y, objectArray[i].z, objectArray[i].bwratio);
-            //std::cout << "F: " << msg->header.frame_id << "  X: " << objectArray[i].x << "  Y: " << objectArray[i].y << "  Z: " << objectArray[i].z << "  R: " << objectArray[i].bwratio << std::endl;
+            if (isnan(objectArray[i].x)) continue;
+            if (isnan(objectArray[i].y)) continue;
+            if (isnan(objectArray[i].z)) continue;
+            if (isnan(objectArray[i].roll)) continue;
+            if (isnan(objectArray[i].pitch)) continue;
+            if (isnan(objectArray[i].yaw)) continue;
 
             geometry_msgs::Pose circlePose;
+            // Convert to ROS Coordinate System
             circlePose.position.x = -objectArray[i].y;
             circlePose.position.y = -objectArray[i].z;
             circlePose.position.z = objectArray[i].x;
+
+            // Convert YPR to Quaternion
             tf::Quaternion q;
             q.setRPY(objectArray[i].roll, objectArray[i].pitch, objectArray[i].yaw);
             circlePose.orientation.x = q.getX();
             circlePose.orientation.y = q.getY();
             circlePose.orientation.z = q.getZ();
             circlePose.orientation.w = q.getW();
+
+            // Add to msg array
+            std::cout << floor(objectArray[i].bwratio) << std::endl;
+            std::cout << startup_time_str << std::endl;
+            std::string toDel = generateUUID(startup_time_str,round(objectArray[i].bwratio));
+            std::cout << toDel << std::endl;
+            std::cout << "=============================" << std::endl;
+            
+            tracked_objects.uuid.push_back(toDel);
             tracked_objects.pose.push_back(circlePose);
             tracked_objects.roundness.push_back(objectArray[i].roundness);
             tracked_objects.bwratio.push_back(objectArray[i].bwratio);
             tracked_objects.esterror.push_back(objectArray[i].esterror);
 
+            // Generate RVIZ marker for visualisation
             visualization_msgs::Marker marker;
             marker.header = msg->header;
             marker.id = floor(objectArray[i].bwratio);
@@ -125,40 +91,50 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
         }
         vis_pub.publish(marker_list);
     }
-    //printf("%s\n", "--------------------");
     memcpy((void*) &msg->data[0], image->data, msg->step * msg->height);
     imdebug.publish(msg);
 }
 
-void cameraInfoCallBack(const sensor_msgs::CameraInfo &msg) {
+void FindCircle::cameraInfoCallBack(const sensor_msgs::CameraInfo &msg) {
     trans->updateParams(msg.K[2], msg.K[5], msg.K[0], msg.K[4]);
 }
 
-int main(int argc, char* argv[]) {
-
-    ros::init(argc, argv, "circle_detector", ros::init_options::AnonymousName);
+FindCircle::FindCircle(void) {
     nh = new ros::NodeHandle("~");
+    defaultImageWidth = 640;
+    defaultImageHeight = 480;
+    circleDiameter = 0.049;
+    startup_time_str = num_to_str<double>(ros::Time::now().toSec());
+}
+
+FindCircle::~FindCircle(void) {
+    delete image;
+    for (int i = 0; i < MAX_PATTERNS; i++) delete detectorArray[i];
+    delete trans;
+}
+
+void FindCircle::init(int argc, char* argv[]) {
 
     if (nh->getParam("camera", topic)) {
         std::transform(topic.begin(), topic.end(), topic.begin(), ::tolower);
     } else {
         if (argc != 2) {
-            std::cerr << "Please supply whether you are subscribing to the left or right camera (R/L)" << std::endl;
-            return -1;
+            ROS_FATAL("Please supply whether you are subscribing to the left or right camera (R/L)");
+            return;
         } else {
             if (argv[1][0] == 'l' || argv[1][0] == 'L') {
                 topic = "left";
             } else if (argv[1][0] == 'r' || argv[1][0] == 'R') {
                 topic = "right";
             } else {
-                std::cerr << "Please supply whether you are subscribing to the left or right camera (R/L)" << std::endl;
-                return -1;
+                ROS_FATAL("Please supply whether you are subscribing to the left or right camera (R/L)");
+                return;
             }
         }
     }
 
     image_transport::ImageTransport it(*nh);
-    ros::Subscriber subcamera = nh->subscribe("/" + topic + "/rgb/camera_info", 1, cameraInfoCallBack);
+    nh->subscribe("/" + topic + "/rgb/camera_info", 1, &FindCircle::cameraInfoCallBack, this);
     image = new CRawImage(defaultImageWidth, defaultImageHeight, 4);
     trans = new CTransformation(circleDiameter, nh);
     for (int i = 0; i < MAX_PATTERNS; i++) {
@@ -166,19 +142,26 @@ int main(int argc, char* argv[]) {
     }
 
     image->getSaveNumber();
-    image_transport::Subscriber subim = it.subscribe("/" + topic + "/rgb/image_mono", 1, imageCallback);
-    imdebug = it.advertise("/circledetection/" + topic + "/rgb/processedimage", 1);
+    image_transport::Subscriber subim = it.subscribe("/" + topic + "/rgb/image_mono", 1, &FindCircle::imageCallback, this);
 
+    imdebug = it.advertise("/circledetection/" + topic + "/rgb/processedimage", 1);
     pubLeft = nh->advertise<circle_detection::STrackedObject>("/circledetection/left_circleArray", 0);
     pubRight = nh->advertise<circle_detection::STrackedObject>("/circledetection/right_circleArray", 0);
-
     vis_pub = nh->advertise<visualization_msgs::MarkerArray>("/circledetection/rviz_marker", 0);
     lookup = new tf::TransformListener();
-
     ROS_DEBUG("Server running");
     ros::spin();
-    delete image;
-    for (int i = 0; i < MAX_PATTERNS; i++) delete detectorArray[i];
-    delete trans;
+}
+
+int main(int argc, char* argv[]) {
+
+    ros::init(argc, argv, "circle_detector", ros::init_options::AnonymousName);
+
+    FindCircle *detector = new FindCircle();
+
+    //Attempt to start detector
+    detector->init(argc, argv);
+    //Clean up
+    detector->~FindCircle();
     return 0;
 }
